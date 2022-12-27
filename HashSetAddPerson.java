6f28d61daeedfd16c1989154a4355b8a32ff5de4
kimyujin <w2117@e-mirim.hs.kr>
2022-12-27 09:49:14 +0900

    HashSet 회원추가 알고리즘

---
+++ HashSetAddPerson.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class HashSetAddPerson{
+    public static void main(String[] args) {
+        HashSet<String> name = new HashSet<String>();
+        Scanner scan = new Scanner(System.in);
+        for(int i = 0; i < 5; i++){
+            System.out.print("추가할 회원의 이름은 : ");
+            name.add(scan.nextLine());
+        }
+        System.out.println(name);
+    }
+}
---
+++ LinkedListTest.java
@@ -0,0 +1,8 @@
+import java.util.LinkedList;
+
+public class LinkedListTest {
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<String>();
+
+    }
+}
