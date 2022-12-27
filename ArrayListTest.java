5f145298e3cb23efe0ccc530d429a6bdf6940e8a
kimyujin <w2117@e-mirim.hs.kr>
2022-12-27 09:36:52 +0900

    ArrayList 리스트 알고리즘

---
+++ ArrayListTest.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Point {
+    int x,y;
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+    public String toString(){ return "("+x+","+y+")";}
+}
+public class ArrayListTest{
+    public static void main(String[] args) {
+        ArrayList<Point> list = new ArrayList<>();
+
+        list.add(new Point(0, 0));
+        list.add(new Point(4, 0));
+        list.add(new Point(3, 5));
+        list.add(new Point(-1, 3));
+        list.add(new Point(13, 2));
+
+        System.out.println(list);
+    }
+}
