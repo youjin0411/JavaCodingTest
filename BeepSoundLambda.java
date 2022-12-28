c8015737d35711957c8131ded9dd9d688a94a27a
kimyujin <w2117@e-mirim.hs.kr>
2022-12-28 15:47:42 +0900

    람다식+Timer클래스활용

---
+++ BeepSoundLambda.java
@@ -0,0 +1,26 @@
+import javax.swing.Timer;
+
+public class BeepSoundLambda {
+    public static void main(String[] args) {
+        //실제 타이머의 기능을 수행하는 클래스
+        Timer t = new Timer(1000, event -> System.out.println("beep"));
+        //Timer클래스가 수행되어야 할 내용을 작성하는 클래스
+//        TimerTask mt = new TimerTask() {
+//            @Override
+//            public void run() {
+//                System.out.println("beep");
+//            }
+//        };
+//        t.schedule(mt, 0, 1000);
+
+        //람다식
+        t.start();
+        for(int i = 0; i < 1000; i++){
+            try {
+                Thread.sleep(1000);
+            }catch (InterruptedException e){
+
+            }
+        }
+    }
+}
---
+++ lambda.java
@@ -0,0 +1,23 @@
+public class lambda {
+    public static void main(String[] args) {
+        //람다식 형식
+        /*
+        () -> System.out.println("Hello World")
+        (String s) -> { System.out.println(s); }
+
+        //람다식을 이용한 방법
+        button.addActionListener( (e) -> {
+            System.out.println("버튼 클릭!");
+         });
+
+         //람다식을 이용한 방법
+         new Thread( () -> System.out.println(
+                            "스레드 실행") ).start();
+
+         //람다식을 이용한 방법
+         List<Integer> list = Array.asList(
+                            1 ,2 ,3 ,4,5);
+         ( () -> System.out.println(list);)
+         */
+    }
+}
